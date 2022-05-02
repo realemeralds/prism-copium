185f9562e67037d7bede6abc38a8aba81f7607ff
Capseeyool <capsulechew@gmail.com>
2022-05-02 19:51:34 +0800

    Refactoring changes

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,33 +1,36 @@
-
-document.addEventListener('DOMContentLoaded', function(event) {
+document.addEventListener('DOMContentLoaded', () => {
     // new element for each row
-    var txt = ["Welcome to", "Catalyst 2022"]
+    var txt = ['Welcome to', 'Catalyst 2022']
     // delay between letters
     let delay = 200
     var index = 0
 
-    function typeWriter(text, i, id, fnCallback) {
-    if (i < (text.length)) {
-        document.getElementById(id).innerHTML = text.substring(0, i+1) +'<span id="caret" aria-hidden="true"></span>';
-        // wait for a while and call this function again for next character
-        setTimeout(function() {
-            typeWriter(text, i + 1, id, fnCallback)}, delay);
-    }
-        // text finished, call callback if there is a callback function
+    typeWriter = (text, i, id, fnCallback) => {
+        if (i < text.length) {
+            document.getElementById(id).innerHTML = text.substring(0, i + 1) + '<span id="caret" aria-hidden="true"></span>'
+            // wait for a while and call this function again for next character
+            setTimeout(() => {
+                typeWriter(text, i + 1, id, fnCallback)}, delay)
+            }
+            // text finished, call callback if there is a callback function
         else {
             index++
-            if (index < (txt.length)) {
-                document.getElementById(id).innerHTML = txt[index-1]
+            if (index < txt.length) {
+                document.getElementById(id).innerHTML = txt[index - 1]
             }
             if (typeof fnCallback == 'function') {
-                setTimeout(fnCallback, delay);
-            }     
-      }
-   } 
-   // main
-   
-   typeWriter(txt[index], 0, "welcome" + String(index+1), function() {
-        typeWriter(txt[index], 0, "welcome" + String(index+1), null)
-        document.getElementById("welcome" + String(index+1)).style.whiteSpace = "pre";
+                setTimeout(fnCallback, delay)
+            }
+        }
+    }
+    // main
+    typeWriter(txt[index], 0, 'welcome' + String(index + 1), () => {
+        typeWriter(txt[index], 0, 'welcome' + String(index + 1), null)
+        document.getElementById('welcom' + String(index + 1)).style.whiteSpace = 'pre'
     })
 })
+
+// changes:
+// changed all quotes to single quotes except in html (feel free to change all to double, i just want to standardize)
+// changed function() {} to () => {} (again, can change but standardize)
+// removed all semicolons
