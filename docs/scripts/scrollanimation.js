aae71feb7b20c7c87b49371cd9ec13e29c5f26e4
Jasper Koh <kohjasper88@gmail.com>
2022-05-07 01:17:27 +0800

    Scroll Animations Added

---
+++ docs/scripts/scrollanimation.js
@@ -0,0 +1,12 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    //reset: true
+})
+
+sr.reveal(`.spk1`)
+sr.reveal(`.spk2`, {delay: 500, origin: 'bottom'})
+sr.reveal(`.spk3`, {delay: 500, origin: 'left'})
+sr.reveal(`.spk4`, {delay: 500, origin: 'right'})
