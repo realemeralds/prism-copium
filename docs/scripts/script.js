d88b9a15ab2cbca47ae00db203e8f4e1e9086ee6
realemeralds <102229990+realemeralds@users.noreply.github.com>
2022-05-08 04:32:44 +0800

    basically everything done. lol

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,38 +1,38 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     // new element for each row
-//     var txt = ['Welcome to', 'Catalyst 2022']
-//     // delay between letters
-//     let delay = 200
-//     var index = 0
+document.addEventListener('DOMContentLoaded', () => {
+    // new element for each row
+    var txt = ['Welcome to', 'Catalyst 2022']
+    // delay between letters
+    let delay = 200
+    var index = 0
 
-//     typeWriter = (text, i, id, fnCallback) => {
-//         if (i < text.length) {
-//             document.getElementById(id).innerHTML = text.substring(0, i + 1) + '<span id="caret" aria-hidden="true"></span>'
-//             // wait for a while and call this function again for next character
-//             setTimeout(() => {
-//                 typeWriter(text, i + 1, id, fnCallback)}, delay)
-//             }
-//             // text finished, call callback if there is a callback function
-//         else {
-//             index++
-//             if (index < txt.length) {
-//                 document.getElementById(id).innerHTML = txt[index - 1]
-//             }
-//             if (typeof fnCallback == 'function') {
-//                 setTimeout(fnCallback, delay)
-//             }
-//         }
-//     }
-//     // main
-//     document.getElementById('welcome' + String(index + 1)).style.whiteSpace = 'pre'
-//     typeWriter(txt[index], 0, 'welcome' + String(index + 1), () => {
-//         typeWriter(txt[index], 0, 'welcome' + String(index + 1), null);
-//         document.getElementById('welcome' + String(index + 1)).style.whiteSpace = 'pre'
-//     })
-// })
+    typeWriter = (text, i, id, fnCallback) => {
+        if (i < text.length) {
+            document.getElementById(id).innerHTML = text.substring(0, i + 1) + '<span id="caret" aria-hidden="true"></span>'
+            // wait for a while and call this function again for next character
+            setTimeout(() => {
+                typeWriter(text, i + 1, id, fnCallback)}, delay)
+            }
+            // text finished, call callback if there is a callback function
+        else {
+            index++
+            if (index < txt.length) {
+                document.getElementById(id).innerHTML = txt[index - 1]
+            }
+            if (typeof fnCallback == 'function') {
+                setTimeout(fnCallback, delay)
+            }
+        }
+    }
+    // main
+    document.getElementById('welcome' + String(index + 1)).style.whiteSpace = 'pre'
+    typeWriter(txt[index], 0, 'welcome' + String(index + 1), () => {
+        typeWriter(txt[index], 0, 'welcome' + String(index + 1), null);
+        document.getElementById('welcome' + String(index + 1)).style.whiteSpace = 'pre'
+    })
+})
 
-document.getElementById('welcome1').innerHTML = 'Welcome to'
-document.getElementById('welcome2').innerHTML = 'Catalyst 2022'
+// document.getElementById('welcome1').innerHTML = 'Welcome to'
+// document.getElementById('welcome2').innerHTML = 'Catalyst 2022'
 
 // changes:
 // changed all quotes to single quotes except in html (feel free to change all to double, i just want to standardize)
