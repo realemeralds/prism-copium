b99c3075a551c3a10b65a52d115f79f6b5f87ba0
Capseeyool <capsulechew@gmail.com>
2022-05-24 08:35:25 +0800

    Fix mobile compatability issues

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,19 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // /* Adding JQuery */
-    // var head = document.getElementsByTagName('head')[0];
-    // var script = document.createElement('script');
-    // script.type = 'text/javascript';
-    // script.src = "http://code.jquery.com/jquery-2.2.1.min.js";
-    // // Then bind the event to the callback function.
-    // // There are several events for cross browser compatibility.
-
-    // // Fire the loading
-    // head.appendChild(script);
-    
     // new element for each row
     var txt = ['Welcome to', 'Catalyst 2022']
     // delay between letters
-    let delay = 200
+    const delay = 200
     var index = 0
 
     typeWriter = (text, i, id, fnCallback) => {
@@ -21,9 +10,10 @@ document.addEventListener('DOMContentLoaded', () => {
             document.getElementById(id).innerHTML = text.substring(0, i + 1) + '<span id="caret" aria-hidden="true"></span>'
             // wait for a while and call this function again for next character
             setTimeout(() => {
-                typeWriter(text, i + 1, id, fnCallback)}, delay)
-            }
-            // text finished, call callback if there is a callback function
+                typeWriter(text, i + 1, id, fnCallback)
+            }, delay)
+        }
+        // text finished, call callback if there is a callback function
         else {
             index++
             if (index < txt.length) {
@@ -34,13 +24,6 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
     }
-    // main
-    // check for overflowing elements (to remove)
-    // document.querySelectorAll('*').forEach(el => {
-    //     if (el.offsetWidth > document.documentElement.offsetWidth) {
-    //         console.log('Found the worst element ever: ', el);
-    //     }
-    //   });
 
     document.getElementById('welcome' + String(index + 1)).style.whiteSpace = 'pre'
     typeWriter(txt[index], 0, 'welcome' + String(index + 1), () => {
--- docs/scripts/scrollanimation.js
+++ docs/scripts/scrollanimation.js
@@ -3,7 +3,6 @@ const sr = ScrollReveal({
     distance: '60px',
     duration: 2500,
     delay: 400,
-    //reset: true
 })
 
 sr.reveal(`.spk1`)
